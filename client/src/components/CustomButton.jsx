fc34838c9a9fe558f919fe6d6f889a88dc58ca8d
Youcef <boukethiryoucef.05@gmail.com>
2023-07-27 18:27:54 +0100

    customizer page v0.1

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -5,32 +5,32 @@ import state from '../store';
 import { getContrastingColor } from '../config/helpers';
 
 const CustomButton = ({ type, title, customStyles, handleClick }) => {
-  const snap = useSnapshot(state);
+    const snap = useSnapshot(state);
 
-  const generateStyle = (type) => {
+    const generateStyle = (type) => {
     if(type === 'filled') {
-      return {
+        return {
         backgroundColor: snap.color,
         color: getContrastingColor(snap.color)
-      }
+        }
     } else if(type === "outline") {
-      return {
-        borderWidth: '1px',
-        borderColor: snap.color,
-        color: snap.color
-      }
+        return {
+            borderWidth: '1px',
+            borderColor: snap.color,
+            color: snap.color
+        }
+        }
     }
-  }
 
-  return (
-    <button
-      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
-      style={generateStyle(type)}
-      onClick={handleClick}
-    >
-      {title}
-    </button>
-  )
-}
+    return (
+        <button
+        className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+        style={generateStyle(type)}
+        onClick={handleClick}
+        >
+        {title}
+        </button>
+    )
+    }
 
 export default CustomButton
